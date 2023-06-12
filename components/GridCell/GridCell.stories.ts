2ff9a350bc422d8e131777ef0dd09bc0b72c4878
Marwen Arfa <marwen.arfa@gmail.com>
2023-06-12 01:15:26 +0100

    chore: fix storybook to integrate Tailwind

---
+++ components/GridCell/GridCell.stories.ts
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { GridCell } from './GridCell';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Example/GridCell',
+  component: GridCell,
+  tags: ['autodocs'],
+  argTypes: {
+    value: {
+      control: {
+        type: 'select',
+        options: ['S', 'X', 'O', ' '],
+      },
+    },
+    onClick: { action: 'clicked' },
+  },
+} satisfies Meta<typeof GridCell>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  args: {
+    value: 'S',
+  },
+};
